a5d07c52a056f410fd4233039e91c72e66055d6c
judiel23 <judielsm@gmail.com>
2016-07-20 15:32:38 -0400

    hola

---
+++ assets/js/scripts.js
@@ -0,0 +1,283 @@
+/*
+Theme Name: IAMX
+Author: Trendy Theme
+Author URL: trendytheme.net
+*/
+
+/*
+    = Preloader
+    = Animated scrolling / Scroll Up
+    = Full Screen Slider
+    = Sticky Menu
+    = Back To Top
+    = Countup
+    = Progress Bar
+    = More skill
+    = Shuffle
+    = Magnific Popup
+    = Vidio auto play
+    = Fit Vids
+    = Google Map
+
+*/
+
+jQuery(function ($) {
+
+    'use strict';
+
+    /* ---------------------------------------------- /*
+     * Preloader
+    /* ---------------------------------------------- */
+var colors=['#E6D5E5','#D5E6D6','#D5DDE6','#E6DED5',];
+    $(window).ready(function() {
+        $('#pre-status').fadeOut();
+        $('#tt-preloader').delay(350).fadeOut('slow');
+    });
+
+$('#con').submit(function(event) {
+    event.preventDefault();
+    var name=$('#InputName1').val();
+    var message=$('#InputTextarea').val();
+
+    $('#coments').append('<div class="w3-card-12" style="background-color:'+colors[Math.floor(Math.random() * colors.length)]+'"><p><h4> <b>Nombre: </b> '+name+'</h4></p><p> <b>Mensaje: </b>'+message+'</p></div>');
+    document.getElementById("con").reset();
+    
+
+});
+
+
+    // -------------------------------------------------------------
+    // Animated scrolling / Scroll Up
+    // -------------------------------------------------------------
+
+    (function () {
+        $('a[href*=#]').bind("click", function(e){
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top
+            }, 1000);
+            e.preventDefault();
+        });
+    }());
+
+
+
+    // -------------------------------------------------------------
+    // Full Screen Slider
+    // -------------------------------------------------------------
+    (function () {
+        $(".tt-fullHeight").height($(window).height());
+
+        $(window).resize(function(){
+            $(".tt-fullHeight").height($(window).height());
+        });
+
+    }());
+
+
+    // -------------------------------------------------------------
+    // Sticky Menu
+    // -------------------------------------------------------------
+
+    (function () {
+        $('.header').sticky({
+            topSpacing: 0
+        });
+
+        $('body').scrollspy({
+            target: '.navbar-custom',
+            offset: 70
+        })
+    }());
+
+
+
+
+    // -------------------------------------------------------------
+    // Back To Top
+    // -------------------------------------------------------------
+
+    (function () {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+    }());
+
+
+    // -------------------------------------------------------------
+    // Countup
+    // -------------------------------------------------------------
+    $('.count-wrap').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).find('.timer').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+
+    // -------------------------------------------------------------
+    // Progress Bar
+    // -------------------------------------------------------------
+ 
+    $('.skill-progress').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'),function(){
+                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            });
+            $(this).unbind('inview');
+        }
+    });
+    
+    // -------------------------------------------------------------
+    // More skill
+    // -------------------------------------------------------------
+    $('.more-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $('.chart').easyPieChart({
+                //your configuration goes here
+                easing: 'easeOut',
+                delay: 3000,
+                barColor:'#68c3a3',
+                trackColor:'rgba(255,255,255,0.2)',
+                scaleColor: false,
+                lineWidth: 8,
+                size: 140,
+                animate: 2000,
+                onStep: function(from, to, percent) {
+                    this.el.children[0].innerHTML = Math.round(percent);
+                }
+
+            });
+            $(this).unbind('inview');
+        }
+    });
+
+
+    // -------------------------------------------------------------
+    // Shuffle
+    // -------------------------------------------------------------
+
+    (function () {
+
+        var $grid = $('#grid');
+
+        $grid.shuffle({
+            itemSelector: '.portfolio-item'
+        });
+
+        /* reshuffle when user clicks a filter item */
+        $('#filter a').click(function (e) {
+            e.preventDefault();
+
+            // set active class
+            $('#filter a').removeClass('active');
+            $(this).addClass('active');
+
+            // get group name from clicked item
+            var groupName = $(this).attr('data-group');
+
+            // reshuffle grid
+            $grid.shuffle('shuffle', groupName );
+        });
+
+
+    }());
+
+
+    // -------------------------------------------------------------
+    // Magnific Popup
+    // -------------------------------------------------------------
+
+    (function () {
+      $('.image-link').magnificPopup({
+
+        gallery: {
+          enabled: true
+        },
+        removalDelay: 300, // Delay in milliseconds before popup is removed
+        mainClass: 'mfp-with-zoom', // this class is for CSS animation below
+        type:'image'
+
+      });
+
+    }());
+
+
+
+    // -------------------------------------------------------------
+    
+
+
+
+    
+
+
+
+
+    // -------------------------------------------------------------
+    // STELLAR FOR BACKGROUND SCROLLING
+    // -------------------------------------------------------------
+
+    $(window).load(function() {
+
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+         
+        }else {
+            $.stellar({
+                horizontalScrolling: false,
+                responsive: true
+            });
+        }
+
+    });
+
+
+    // -------------------------------------------------------------
+    // WOW JS
+    // -------------------------------------------------------------
+
+    (function () {
+
+        new WOW({
+
+            mobile:  false
+
+        }).init();
+
+    }());
+
+    // -------------------------------------------------------------
+    // SKILLS
+    // -------------------------------------------------------------
+(function () {
+
+    
+       for (var i = 0; i < data.length; i++) {
+        $('#skills').append('<div class="skill-progress col-md-offset-2"<div class="skill-title"><img></img><h3>'+data[i].titulo+'</h3><p>'+data[i].tiempo+'</p></div>');
+        $('#skills').append(' <div class="progress col-md-offset-1"><div class="progress-bar six-sec-ease-in-out" role="progressbar" aria-valuenow="'+data[i].porcentaje+'" aria-valuemin="0" aria-valuemax="100" ><span>90%</span></div></div>');
+           
+       }
+    
+
+    }());
+
+
+});
+
+
+
+
+
